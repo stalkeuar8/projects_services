8bcdfc7b6ba86c7a69907cdcbb08f48268ff4a8e
artem <sutalkuar808typ@gmail.com>
2026-03-06 17:50:28 +0000

    booking_service: docker and git changes

---
+++ booking_service_backend/app/models/base.py
@@ -0,0 +1,7 @@
+from sqlalchemy import MetaData, ForeignKey, text
+from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
+from typing import Annotated
+import datetime
+
+
+print("hello")
