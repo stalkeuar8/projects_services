4e8816fed883b7a506edee8782003fae76a207d0
artem <sutalkuar808typ@gmail.com>
2026-03-11 18:04:06 +0000

    booking_service: paginator

--- booking_service_backend/app/services/background_processes.py
+++ booking_service_backend/app/services/background_processes.py
@@ -10,16 +10,15 @@ class BackgroundCleaner:
     async def background_bookings_cleaner(time_frequency_mins: int = 5):
         while True:
             try: 
-                async with async_session_factory() as session:
+                async with async_session_factory.begin() as session:
                         query = (
                             delete(Bookings)
                             .where(Bookings.status=='pending')
                         )
 
                         await session.execute(query)
-                        await session.commit()
 
-            except Exception as e:
+            except Exception as e:  
                 print(f"Background bookings cleaner ERROR: {e}")
 
             await asyncio.sleep(time_frequency_mins*60)    
---
+++ booking_service_backend/app/utils/paginator.py
@@ -0,0 +1,35 @@
+
+
+class ResultsPaginator:
+
+    def __init__(self, results: list, results_per_page: int = 5):
+        self.current_group = -1
+        self.groups = [
+            results[i:i + results_per_page] 
+            for i in range(0, len(results), results_per_page)
+        ]
+        
+
+    def __iter__(self):
+        return self
+    
+
+    def __next__(self):
+        if not self.current_group > len(self.groups) - 2:
+            self.current_group += 1
+            next_group = self.groups[self.current_group]
+            return next_group
+        else:
+            raise IndexError("You are on the last page, there is no next pages")
+    
+            
+    def previous(self):
+        if self.current_group == 0:
+            raise IndexError("You are on the first page, there is no previous page.")
+            
+        if self.current_group == -1:
+            raise IndexError("Pages were never printed.")
+            
+        self.current_group -= 1
+        previous_group = self.groups[self.current_group]
+        return previous_group
